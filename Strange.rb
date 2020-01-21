9806b9dbb230050401105e736a0efb26cbcc417b
Kendelltaylor <45106796+Kendelltaylor@users.noreply.github.com>
2020-01-21 11:03:55 -0800

    Add files via upload

---
+++ Strange.rb
@@ -0,0 +1,19 @@
+use_bpm 180
+live_loop:Strange do
+  play:C4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:C5
+  sleep 0.5
+  play:B4
+  sleep 0.5
+  play:G4
+  sleep 0.5
+  play:E4
+  sleep 0.5
+end
